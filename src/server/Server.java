888cacc6863c8ebe75134eebadae71c749e30aa3
Askar <nurbekovaskar099@gmail.com>
2026-02-17 23:10:33 +0600

    Created methods from lesson

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,29 +1,20 @@
 package server;
 
 import com.sun.net.httpserver.HttpServer;
+import com.sun.tools.javac.Main;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class Server {
-
-    private static HttpServer makeServer() throws IOException {
+    public static HttpServer makeServer() throws IOException {
         String host = "localhost";
-        InetSocketAddress address = new InetSocketAddress(8080);
+        InetSocketAddress address = new InetSocketAddress(host,8080);
 
-        String msg = "запускаем сервер по адресу\" + \" http://%s:%s/%n";
+        String msg = "запускаем сервер по адресу" + " http://%s:%s/%n";
         System.out.printf(msg, address.getHostName(), address.getPort());
         HttpServer server = HttpServer.create(address, 50);
         System.out.println("  удачно!");
         return server;
-    }
-
-    public void run() {
-        try {
-            HttpServer server = makeServer();
-            server.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
