4c589603252612f5a978c685f603e3edb709b468
Askar <nurbekovaskar099@gmail.com>
2026-02-19 16:52:41 +0600

    Minimized code from main to another class handle

--- src/Main.java
+++ src/Main.java
@@ -1,110 +1,17 @@
-import com.sun.net.httpserver.Headers;
-import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
-import server.Server;
 
 import java.io.*;
-import java.net.URI;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-
 import static server.Server.makeServer;
 
 public class Main {
     public static void main(String[] args) {
         try {
             HttpServer server = makeServer();
-            initRoutes(server);
+            Handle.initRoutes(server);
             server.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    private static void initRoutes(HttpServer server) {
-        server.createContext("/", Main::handleRootRequest);
-        server.createContext("/apps/", Main::handleAppsRequest);
-        server.createContext("/apps/profile", Main::handleProfileRequest);
-    }
-
-    private static void handleRequest(HttpExchange exchange, String msg) {
-        try {
-            exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=utf-8");
-            int responseCode = 200;
-            int length = 0;
-            exchange.sendResponseHeaders(responseCode, length);
-
-            try (PrintWriter writer = getWriterFrom(exchange)) {
-                String method = exchange.getRequestMethod();
-                URI uri = exchange.getRequestURI();
-                String ctxPath = exchange.getHttpContext().getPath();
-                write(writer, "HTTP Метод", method);
-                write(writer, "Запрос", uri.toString());
-                write(writer, "Обработан через", ctxPath);
-
-                writeHeaders(writer, "Заголовки запроса", exchange.getRequestHeaders());
-                writeData(writer, exchange);
-                writer.flush();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void handleRootRequest(HttpExchange exchange) {
-        handleRequest(exchange, "Hello :) It's Root");
-    }
-
-    private static void handleAppsRequest(HttpExchange exchange){
-        handleRequest(exchange, "Hello :) It's App");
-    }
-
-    private static void handleProfileRequest(HttpExchange exchange){
-        handleRequest(exchange, "Hello :) It's Profile");
-    }
-
-    private static PrintWriter getWriterFrom(HttpExchange exchange) {
-        OutputStream output = exchange.getResponseBody();
-        Charset charset = StandardCharsets.UTF_8;
-        return new PrintWriter(output, false, charset);
-    }
-
-    private static void write(Writer writer, String msg, String method) {
-        String data = String.format("%s: %s%n%n", msg, method);
-        try {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void writeHeaders(Writer writer, String type, Headers headers) {
-        write(writer, type, "");
-        headers.forEach((k, v) -> write(writer, "\t" + k, v.toString()));
-    }
-
-    private static BufferedReader getReader(HttpExchange exchange) {
-        InputStream input = exchange.getRequestBody();
-        Charset charset = StandardCharsets.UTF_8;
-        InputStreamReader isr = new InputStreamReader(input, charset);
-        return new BufferedReader(isr);
-    }
-
-    private static void writeData(Writer writer, HttpExchange exchange) {
-        try (BufferedReader reader = getReader(exchange)) {
-            if (!reader.ready()) {
-                return;
-            }
-            write(writer, "Блок данных", "");
-            reader.lines().forEach(v -> write(writer, "\t", v));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-
-
-
 
 }
